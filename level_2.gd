14ab3a27c11acca4e9b0f7208bf51d8c251117f9
Alisha <21amessingham@wakatipu.school.nz>
2024-06-25 11:27:27 +1200

    Level Two Complete
    Completed level two.

---
+++ level_2.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+func _on_exit_button_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
+func _on_area_2d_body_entered(body):
+	if body == $Player:
+		get_tree().change_scene_to_file("res://end.tscn")
