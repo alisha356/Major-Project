08849ec43ca0ac587e8402eebd39c4bf406a7c39
Alisha <21amessingham@wakatipu.school.nz>
2024-06-26 09:59:56 +1200

    Level Three Complete
    Finished level three and moved the backgrounds to a new folder.

--- level_3.gd
+++ level_3.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 
+
 func _on_exit_button_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
 
 
-func _on_area_2d_body_entered(body):
+func _on_end_door_body_entered(body):
 	if body == $Player:
-		get_tree().change_scene_to_file("res://level_3.tscn")
+		get_tree().change_scene_to_file("res://end.tscn")
