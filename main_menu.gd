d70caac3b9dd759714b79ab4a56acfc9de215364
Alisha <21amessingham@wakatipu.school.nz>
2024-06-27 10:44:40 +1200

    How to
    Added instructions on the main menu so the player can see how to play the game is they're unsure.

--- main_menu.gd
+++ main_menu.gd
@@ -13,3 +13,7 @@ func _process(delta: float):
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://level_1.tscn")
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://how_to.tscn")
