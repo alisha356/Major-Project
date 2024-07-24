bc10f40772b901e1c4a2f8f5231678ffbddc9e1a
Alisha <21amessingham@wakatipu.school.nz>
2024-07-25 11:00:11 +1200

    Clarification
    I added a couple of sentences to the how-to page clarifying that the arrows are a guide and that you shouldn't touch the ground.

--- main_menu.gd
+++ main_menu.gd
@@ -15,5 +15,5 @@ func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://level_1.tscn")
 
 
-func _on_button_pressed():
+func _on_howto_pressed():
 	get_tree().change_scene_to_file("res://how_to.tscn")
