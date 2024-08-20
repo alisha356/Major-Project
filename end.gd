8499943a1b6ee687a4de91bfc0b262e423c588ea
Alisha <21amessingham@wakatipu.school.nz>
2024-08-21 09:35:08 +1200

    Timer, score, and deaths update
    Displayed the timer, score, and deaths variables after the user finishes the level.

--- end.gd
+++ end.gd
@@ -1,5 +1,12 @@
 extends Node2D
+@onready var score = $"Score Text"
+@onready var deaths = $"Deaths Text"
+@onready var time = $"Time Text"
 
+func _ready():
+	time.text = "Time; " + str(global.time)
+	score.text = "Score; " + str(global.score)
+	deaths.text = "Deaths; " + str(global.deaths)
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
--- level_intermission.gd
+++ level_intermission.gd
@@ -1,4 +1,12 @@
 extends Node2D
+@onready var score = $"Score Text"
+@onready var deaths = $"Deaths Text"
+@onready var time = $"Time Text"
+
+func _ready():
+	time.text = "Time; " + str(global.time)
+	score.text = "Score; " + str(global.score)
+	deaths.text = "Deaths; " + str(global.deaths)
 
 
 func _on_continue_pressed():
@@ -7,3 +15,4 @@ func _on_continue_pressed():
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
+
